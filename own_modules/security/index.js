d00ad89fe6933d61fac31d198a2f5405fef0ba5e
Michael Fernandez <michael_f2005@hotmail.com>
2020-05-14 10:20:19 -0500

    1. Agregar a .gitignore archivo para probar modulos propios.
    2. Implementación modulos seguridad.
    2.1. encodePassword: Encriptar contraseña.
    2.2. comparePassword: comparar contraseña en texto plana y encriptada, para confirmas si son iguales.
    2.3. generateToken: Generar token. (id, name, username).
    2.4. verifyToken: Verificar token. Genera warning en audit.log en caso de error.

---
+++ own_modules/security/index.js
@@ -0,0 +1,45 @@
+// MODULOS DE TERCEROS
+// importar bcrypt
+const bcrypt = require("bcrypt");
+// importar jsonwebtoken
+const jwt = require('jsonwebtoken');
+
+// MODULOS PROPRIOS
+// importar configuracion
+const config = require('../config');
+// importar log
+const log = require('../log');
+
+const security = {
+    encodePassword: (plainPassword) => {
+        const salt = bcrypt.genSaltSync(config.saltRounds);
+        return bcrypt.hashSync(plainPassword, salt);
+    },
+    comparePassword: (plainPassword, encryptedPassword) => {
+        return bcrypt.compareSync(plainPassword, encryptedPassword);
+    },
+    generateToken: (user) => {
+        const tokenUser = {
+            id: user.id,
+            username: user.username,
+            name: user.name
+        };
+    
+        const token = jwt.sign(tokenUser, config.tokenKey, {
+            expiresIn: config.tokenExpiresIn
+        });
+    
+        return token;
+    },
+    verifyToken: (token) => { 
+        return jwt.verify(token, config.tokenKey, (err, user) => {
+            if(err){
+                log.warn(`Token ${token} inválido. Error:${err.name}`);
+                return false;
+            }
+            return true;
+        });
+    }
+};
+
+module.exports = security;
